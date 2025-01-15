924271dfda26fb6917df26ea90146864add18ce4
Gregory Hostin <greghostin@icloud.com>
2025-01-15 11:15:42 +0100

    Adding image change on arrow click

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,37 @@
 const slides = [
   {
-    image: "slide1.jpg",
+    image: "./assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
+    image: "./assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: "./assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: "./assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
 
-let flecheGauche = document.querySelector(".arrow_left");
-let flecheDroite = document.querySelector(".arrow_right");
+let arrowLeft = document.querySelector(".arrow_left");
+let arrowRight = document.querySelector(".arrow_right");
 let containerDots = document.querySelector(".dots");
+let activeSlideIndex = 0;
+let imageCarousel = document.querySelector(".banner-img");
 
-flecheGauche.addEventListener("click", () => {
-  console.log("Click gauche OK");
+arrowLeft.addEventListener("click", () => {
+  activeSlideIndex--;
+  imageCarousel.src = slides[activeSlideIndex].image;
 });
 
-flecheDroite.addEventListener("click", () => {
-  console.log("Click droit OK");
+arrowRight.addEventListener("click", () => {
+  activeSlideIndex++;
+  imageCarousel.src = slides[activeSlideIndex].image;
 });
 
 for (i = 0; i < slides.length; i++) {
